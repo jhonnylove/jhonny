ce17547f7fcf84c7f859bdb77d304a8d51b31ef0
jhonnylove <44403851+jhonnylove@users.noreply.github.com>
2018-10-23 11:27:28 -0300

    Add files via upload
    Hello word

---
+++ aqui.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+	printf("\t   l    l    l \n");
+	printf("\t-----------------\n");
+	printf("\t   l    l    l \n");
+	printf("\t-----------------\n");
+	printf("\t   l    l    l \n");
+	system("pause");
+
+	return 0;
+
+
+}
+
